bafddb1b87d34f440831ebd63bcab80a9f811f93
Mikeparra11 <mparra780@uan.edu.co>
2019-03-27 11:46:55 -0500

    first commit

--- config/db.php
+++ config/db.php
@@ -1,7 +1,7 @@
 <?php
 /*Datos de conexion a la base de datos*/
 define('DB_HOST', 'localhost');//DB_HOST:  generalmente suele ser "127.0.0.1"
-define('DB_USER', 'root');//Usuario de tu base de datos
-define('DB_PASS', '');//Contraseña del usuario de la base de datos
+define('DB_USER', 'mike');//Usuario de tu base de datos
+define('DB_PASS', '12345');//Contraseña del usuario de la base de datos
 define('DB_NAME', 'pacrim');//Nombre de la base de datos
 ?>
